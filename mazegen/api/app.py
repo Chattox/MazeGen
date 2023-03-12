745b9613ec79191952aecf5dee059ac7cefc4595
Chattox <conradlineker@gmail.com>
2023-03-12 20:50:13 +0000

    build(api): CORS

--- mazegen/api/app.py
+++ mazegen/api/app.py
@@ -1,10 +1,14 @@
 """ Flask API to generate mazes from a front end """
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 from maze.maze import gen_maze
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/get-maze", methods=["GET"])
+@cross_origin()
 def get_maze():
     """ Return completed maze from given height and width """
     height = int(request.args['height'])
