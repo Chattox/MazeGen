66923f3fc2d37562068c38742d27c13e01ba58b5
Chattox <conradlineker@gmail.com>
2023-03-16 22:16:28 +0000

    refactor(maze): maze class

--- mazegen/api/app.py
+++ mazegen/api/app.py
@@ -1,7 +1,7 @@
 """ Flask API to generate mazes from a front end """
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
-from maze.maze import gen_maze
+from maze.maze import Maze
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -11,6 +11,5 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin()
 def get_maze():
     """ Return completed maze from given height and width """
-    height = int(request.args['height'])
-    width = int(request.args['width'])
-    return gen_maze(height, width)
+    maze = Maze(request.args['height'], request.args['width'], request.args['hasCentralRoom'])
+    return maze.gen_maze()
