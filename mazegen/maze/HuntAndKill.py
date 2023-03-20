f801cb89f18f3beb2836840f27bc7217e0571bbb
Chattox <conradlineker@gmail.com>
2023-03-20 22:41:25 +0000

    feat: randomly generated rooms

--- mazegen/maze/HuntAndKill.py
+++ mazegen/maze/HuntAndKill.py
@@ -26,6 +26,8 @@ class HuntAndKill:
             curr_y, curr_x = self.hunt(num_trials)
             num_trials += 1
 
+        self.connect_maze()
+
         self.maze = self.grid
 
     def set_start_pos(self):
@@ -44,11 +46,17 @@ class HuntAndKill:
             room_start_y = randrange(1, (self.height - room_height) - 1, 2)
             room_end_x = room_start_x + room_width
             room_end_y = room_start_y + room_height
+
+            attempts = 0
             while self.has_overlap((room_start_y, room_start_x), (room_end_y, room_end_x)):
                 room_start_x = randrange(1, (self.width - room_width) - 1, 2)
                 room_start_y = randrange(1, (self.height - room_height) - 1, 2)
                 room_end_x = room_start_x + room_width
                 room_end_y = room_start_y + room_height
+                if attempts > 100:
+                    break
+                else:
+                    attempts += 1
             
             for y in range(room_start_y, room_end_y):
                 for x in range(room_start_x, room_end_x):
@@ -59,7 +67,6 @@ class HuntAndKill:
         for y in range(top_left[0], bottom_right[0]):
             for x in range(top_left[1], bottom_right[1]):
                if self.grid[y, x] == 0:
-                    print('overlap found')
                     return True
     
     def walk(self, y, x):
@@ -142,3 +149,86 @@ class HuntAndKill:
                     exp_maze[y][x] = '.'
         
         return exp_maze
+    
+    def connect_maze(self):
+        self.fix_disjointed_paths(self.get_paths())
+    
+    def get_paths(self):
+        paths = []
+
+        for y in range(1, self.height, 2):
+            for x in range(1, self.width, 2):
+                neighbours = self.find_open_neighbours((y, x))
+                current = set(neighbours + [(y, x)])
+
+                found = False
+                for i, path in enumerate(paths):
+                    intersect = current.intersection(path)
+                    if len(intersect) > 0:
+                        paths[i] = paths[i].union(current)
+                        found = True
+                        break
+
+                if not found:
+                    paths.append(current)
+
+        return self.join_intersections(paths)
+
+    def find_open_neighbours(self, pos):
+        y, x = (pos)
+        neighbours = []
+
+        if y > 1 and self.grid[y - 1][x] == 0 and self.grid[y - 2][x] == 0:
+            neighbours.append((y - 2, x))
+        if (
+            y < self.height - 2
+            and self.grid[y + 1][x] == 0
+            and self.grid[y + 2][x] == 0
+        ):
+            neighbours.append((y + 2, x))
+        if x > 1 and self.grid[y][x - 1] == 0 and self.grid[y][x - 2] == 0:
+            neighbours.append((y, x - 2))
+        if (
+            x < self.width - 2
+            and self.grid[y][x + 1] == 0
+            and self.grid[y][x + 2] == 0
+        ):
+            neighbours.append((y, x + 2))
+
+        shuffle(neighbours)
+        return neighbours
+    
+    def join_intersections(self, sets):
+        for i in range(len(sets) - 1):
+            if sets[i] is None:
+                continue
+
+            for j in range(i + 1, len(sets)):
+                if sets[j] is None:
+                    continue
+                intersect = sets[i].intersection(sets[j])
+                if len(intersect) > 0:
+                    sets[i] = sets[i].union(sets[j])
+                    sets[j] = None
+
+        return list(filter(lambda l: l is not None, sets))
+    
+    def fix_disjointed_paths(self, disjointed):
+        while len(disjointed) > 1:
+            found = False
+            while not found:
+                cell = choice(list(disjointed[0]))
+                neighbours = self.find_neighbours(cell[0], cell[1])
+                for path in disjointed[1:]:
+                    intersect = [c for c in neighbours if c in path]
+
+                    if len(intersect) > 0:
+                        mid = self.midpoint(intersect[0], cell)
+                        self.grid[mid[0], mid[1]] = 0
+                        disjointed[0] = disjointed[0].union(path)
+                        disjointed.remove(path)
+                        found = True
+                        break
+
+    def midpoint(self, a, b):
+        return ((a[0] + b[0]) // 2, (a[1] + b[1]) // 2)
